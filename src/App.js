6617d87e422d58afb74908046486fe0ba4da3d5b
Marlen272 <marlen.04.29.07@gmail,com>
2024-05-22 17:32:23 +0600

    Описание изменений

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [storedUser, setStoredUser] = useState(null);
+
+    useEffect(() => {
+        const name = prompt('Введите ваше имя:');
+        const lastname = prompt('Введите вашу фамилию:');
+
+        if (storedUser) {
+            alert(name === storedUser.name && lastname === storedUser.lastname ? 'Успешно' : 'Это не ты');
+        } else {
+            setStoredUser({ name, lastname });
+        }
+    }, [storedUser]);
+
+    return null;
+};
 
 export default App;
